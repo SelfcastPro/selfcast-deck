02842496af7179da00d07d5b42e9772b3a2fe8c4
SelfcastPro <info@selfcast.com>
2025-09-23 15:17:32 +0200

    Create next.config.mjs

---
+++ talentscout/next.config.mjs
@@ -0,0 +1,12 @@
+## `next.config.mjs`
+```js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+experimental: {
+serverActions: {
+allowedOrigins: ["*"]
+}
+}
+};
+export default nextConfig;
+```
