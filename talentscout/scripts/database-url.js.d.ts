3d3e59140ca3e2fe5336b494a5d2f9722ba4ba94
SelfcastPro <info@selfcast.com>
2025-09-24 14:55:04 +0200

    Create database-url.js.d.ts

---
+++ talentscout/scripts/database-url.js.d.ts
@@ -0,0 +1,2 @@
+export declare function isValidDatabaseUrl(value: string | null | undefined): value is string;
+export declare function normalizeDatabaseUrl(value: string | null | undefined): string | undefined;
