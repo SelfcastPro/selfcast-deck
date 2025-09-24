84899c4bf9019ea59b56281d10f994e9e91024fd
SelfcastPro <info@selfcast.com>
2025-09-24 15:09:46 +0200

    Delete talentscout directory

--- talentscout/scripts/database-url.js.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function isValidDatabaseUrl(value: string | null | undefined): value is string;
-export declare function normalizeDatabaseUrl(value: string | null | undefined): string | undefined;
